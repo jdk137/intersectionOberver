7b96dac786c65aa0cc708b257bde3cc8095891c8
dongke jiang <happyjdk@gmail.com>
2020-02-02 14:52:55 +0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # intersectionOberver
+
+[在线demo](https://jdk137.github.io/intersectionOberver/)
+
+![页面截图](./img/pageClip.png)
+
+```js
+    // 导航条如果滚动出了页面可视区域，就在顶部固定出现导航条的浮层。
+    var io = new IntersectionObserver(
+      function (entries) {
+        entries.forEach(function (i) {
+          // console.log('Time: ' + i.time);
+          // console.log('Target: ' + i.target.id);
+          // console.log('IntersectionRatio: ' + i.intersectionRatio);
+          // console.log('rootBounds: ' + i.rootBounds);
+          // console.log(i.boundingClientRect);
+          // console.log(i.intersectionRect);
+          // console.log(i.isIntersecting);
+          // console.log('================');
+          if (i.target.id === 'staticNav') {
+            if (i.intersectionRatio >= .99) {
+              $('#moveNav').hide();
+            } else {
+              $('#moveNav').show();
+            }
+          }
+        });
+      },
+      {
+        threshold: [0, .5, .99],
+      }
+    );
+    io.observe(document.querySelector('#staticNav'));
+
+    // 页面一共3段，每一段居中时，都会触发相应导航条的高亮
+    ['charts', 'news', 'prevent'].forEach(function (d) {
+      var io = new IntersectionObserver(
+        function (entries) {
+          entries.forEach(function (i) {
+            if (i.isIntersecting || i.intersectionRatio > 0) {
+              highlightNav(d);
+            }
+          });
+        },
+        {
+          threshold: [0],
+          rootMargin: -window.innerHeight / 2.2 + "px 0px" // only vertical center will be intersected
+        }
+      );
+      io.observe(document.querySelector('#' + d));
+    });
+```
+
--- js/intro.js
+++ js/intro.js
@@ -53,10 +53,9 @@ $(document).ready(function () {
       location.href = location.href.split('#')[0] + '#' + type;
     };
 
-    // IntersectionObserver
+    // 导航条如果不能完整显示，就在页面顶部出现导航的浮层。
     var io = new IntersectionObserver(
       function (entries) {
-        // console.log(entries.length);
         entries.forEach(function (i) {
           // console.log('Time: ' + i.time);
           // console.log('Target: ' + i.target.id);
@@ -81,6 +80,7 @@ $(document).ready(function () {
     );
     io.observe(document.querySelector('#staticNav'));
 
+    // 页面一共3段，每一段居中时，都会触发相应导航条的高亮
     ['charts', 'news', 'prevent'].forEach(function (d) {
       var io = new IntersectionObserver(
         function (entries) {
@@ -92,67 +92,12 @@ $(document).ready(function () {
         },
         {
           threshold: [0],
-          rootMargin: -window.innerHeight / 2.2 + "px 0px"
+          rootMargin: -window.innerHeight / 2.2 + "px 0px" // only vertical center will be intersected
         }
       );
       io.observe(document.querySelector('#' + d));
     });
 
-    // var io2 = new IntersectionObserver(
-    //   function (entries) {
-    //     entries.forEach(function (i) {
-    //       if (i.isIntersecting || i.intersectionRatio > 0) {
-    //         highlightNav('charts');
-    //       }
-    //     });
-    //   },
-    //   {
-    //     threshold: [0],
-    //     rootMargin: -window.innerHeight / 2.2 + "px 0px"
-    //   }
-    // );
-    // io2.observe(document.querySelector('#charts'));
-
-
-    // var io3 = new IntersectionObserver(
-    //   function (entries) {
-    //     console.log(entries.length);
-    //     entries.forEach(function (i) {
-    //       console.log('IntersectionRatio3: ' + i.intersectionRatio);
-    //       console.log(i.isIntersecting);
-    //       if (i.target.id === 'news') {
-    //         if (i.intersectionRatio > 0) {
-    //           highlightNav('news');
-    //         }
-    //       }
-    //     });
-    //   },
-    //   {
-    //     threshold: [0],
-    //     rootMargin: -window.innerHeight / 2.2 + "px 0px"
-    //   }
-    // );
-    // io3.observe(document.querySelector('#news'));
-
-    // var io4 = new IntersectionObserver(
-    //   function (entries) {
-    //     console.log(entries.length);
-    //     entries.forEach(function (i) {
-    //       console.log('IntersectionRatio4: ' + i.intersectionRatio);
-    //       console.log(i.isIntersecting);
-    //       if (i.target.id === 'prevent') {
-    //         if (i.intersectionRatio > 0) {
-    //           highlightNav('prevent');
-    //         }
-    //       }
-    //     });
-    //   },
-    //   {
-    //     threshold: [0],
-    //     rootMargin: -window.innerHeight / 2.2 + "px 0px"
-    //   }
-    // );
-    // io4.observe(document.querySelector('#prevent'));
 
   }());
 
